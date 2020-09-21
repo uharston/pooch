51f5385329a2c607ccfc93c85981a6b6f2e93664
uharston <uharston@gmail.com>
2020-09-21 15:01:44 -0500

    added linkl to the homepage

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -5,6 +5,7 @@ class NavBar extends Component {
     render() {
         return(
             <div className='navbar'>
+                <NavLink to='/'>Home</NavLink>
                 <NavLink to='/search'>Search</NavLink>
                 Pooch Logo Left - Sign Up Right - Log In Right
             </div>
