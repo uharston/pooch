29e9d8f46dc01eb7092db23775031aa852cb5051
uharston <uharston@gmail.com>
2020-09-21 11:19:00 -0500

    made landing page container

---
+++ front-end/src/containers/LandingPageContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+class LandingPageContainer extends Component {
+
+    render() {
+        return(
+            <div className='landing-page-container'>
+                
+            </div>
+        )
+    }
+}
+
+export default LandingPageContainer
