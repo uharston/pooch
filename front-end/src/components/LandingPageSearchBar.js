bc265a9497f89109ab624bac425d6066e2ac15c7
uharston <uharston@gmail.com>
2020-09-21 16:42:11 -0500

    Implement Redirect

--- front-end/src/components/LandingPageSearchBar.js
+++ front-end/src/components/LandingPageSearchBar.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'; 
+import { Redirect } from 'react-router-dom'; 
 
 class LandingPageSearchBar extends Component {
 
     state = {
-        text: ''
+        query: '',
+        submitted: false
     }
 
     handleChange = (event) => {
         this.setState({
-            text: event.target.value
+            query: event.target.value
         })
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.setState({
+            submitted: true 
+        })
+    }
+
+    redirect = (query) => {
+        if(this.state.submitted) {
+            return <Redirect to={"/search/" + query} />
+        }
     }
 
     render() {
         return(
             <div className="landing-page-search-bar">
+                <p> Search by: </p> 
+                <button>Breed</button>
+                <button>Location</button>
                 <form onSubmit={ (event) => this.handleSubmit(event) }>
                     <label>Enter location: </label>
                     <input type='text' onChange={ (e) => this.handleChange(e) } />
                     <input type='submit' value='Go'/>
                 </form>
+                
+                { this.redirect(this.state.query) }
             </div>
         )
     }
