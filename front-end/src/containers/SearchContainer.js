2196f75973bdd4ffb4aa3db1d35baecc02b2d979
uharston <uharston@gmail.com>
2020-09-21 14:49:10 -0500

    Created Search Container

---
+++ front-end/src/containers/SearchContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SearchContainer extends Component {
+
+    render() {
+        return(
+            <div className='search-container'>
+                Search Container
+            </div>
+        )
+    }
+}
+
+export default SearchContainer
