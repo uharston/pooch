0a97755a7802d12e97d82c8c5b9abb7b1bab5fc9
uharston <uharston@gmail.com>
2020-09-21 14:19:38 -0500

    convert to class with state - handleChange event

--- front-end/src/components/LandingPageSearchBar.js
+++ front-end/src/components/LandingPageSearchBar.js
@@ -1,15 +1,28 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 
-const LandingPageSearchBar = () => {
-    return(
-        <div className="landing-page-search-bar">
-            <form>
-                <label>Enter location: </label>
-                <input type='text' />
-                <input type='submit' value='Go'/>
-            </form>
-        </div>
-    )
+class LandingPageSearchBar extends Component {
+
+    state = {
+        text: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    render() {
+        return(
+            <div className="landing-page-search-bar">
+                <form>
+                    <label>Enter location: </label>
+                    <input type='text' onChange={ (e) => this.handleChange(e) }/>
+                    <input type='submit' value='Go'/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default LandingPageSearchBar
