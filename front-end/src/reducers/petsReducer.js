8052d1f1a69ddaf12dad917575a6277d8fe2c9bb
uharston <uharston@gmail.com>
2020-09-21 12:32:11 -0500

    added default switch statements

--- front-end/src/reducers/petsReducer.js
+++ front-end/src/reducers/petsReducer.js
@@ -1,5 +1,12 @@
 const petsReducer = ( state = [], action ) => {
-    return state 
+    switch(action.type) {
+        case 'LOADING_PETS': 
+            return state 
+        case 'ADD_PETS': 
+            return state 
+        default: 
+            return state 
+    }
 }
 
 export default petsReducer
