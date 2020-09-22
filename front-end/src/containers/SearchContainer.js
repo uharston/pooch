b804aee95e692421b23f9587cbc1145fb362c46a
uharston <uharston@gmail.com>
2020-09-22 12:33:49 -0500

    connected to store - dissected url for fetch call

--- front-end/src/containers/SearchContainer.js
+++ front-end/src/containers/SearchContainer.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
+import { fetchPets } from '../actions/petsAction';
+
 
 
 class SearchContainer extends Component {
 
+    componentDidMount() {
+        let path = this.props.location.pathname.split('/')
+        let param = path[ path.length - 1 ]
+        this.props.fetchPets(param)
+        debugger
+        // CALL DISPATCH 
+        // DO FETCH CALL
+    }
+
     render() {
+        
         return(
             <div className='search-container'>
                 Search Container
+                {this.props.location.pathname}
             </div>
         )
     }
 }
 
-export default connect() (SearchContainer)
+const mapStateToProps = state => {
+    return state 
+}
+
+export default connect (mapStateToProps, { fetchPets } ) (SearchContainer)
