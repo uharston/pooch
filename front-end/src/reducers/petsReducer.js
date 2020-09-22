df0aa71e641f11c6c3a2bb61963ba8eacafd30b6
uharston <uharston@gmail.com>
2020-09-22 15:22:02 -0500

    Setup up return state

--- front-end/src/reducers/petsReducer.js
+++ front-end/src/reducers/petsReducer.js
@@ -1,10 +1,18 @@
-const petsReducer = ( state = { cats: [], loading: false}, action ) => {
+const petsReducer = ( state = { pets: [], loading: false}, action ) => {
    
     switch(action.type) {
         case 'LOADING_PETS': 
-            return state 
+            return { 
+                ...state, 
+                pets: [ ...state.pets ], 
+                loading: true
+            }
         case 'ADD_PETS': 
-            return state 
+            return {
+                ...state, 
+                pets: action.pets.animals, 
+                loading: false
+            }
         default: 
             return state 
     }
