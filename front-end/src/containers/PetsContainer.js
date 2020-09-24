738e31e546497283c7ae5b2d68a28f089c83cf46
uharston <uharston@gmail.com>
2020-09-24 11:19:03 -0500

    Deleted un-needed code

--- front-end/src/containers/PetsContainer.js
+++ front-end/src/containers/PetsContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchPets } from '../actions/petsAction';
 
@@ -9,42 +8,9 @@ import PetsPagination from '../components/PetsPagination'
 
 class PetsContainer extends Component {
 
-
-
     componentDidMount() {
-        let path = this.props.location.pathname.split('/')
-        if( path.slice(-1)[0].includes('page') ) {
-            let params = Object.assign( {}, {city: path[path.length - 2], state: path[path.length - 3], page: path[path.length - 1]} )
-            let url = `https://api.petfinder.com/v2/animals?location=${this.props.match.params.city},%20${this.props.match.params.state}&type=dog${this.props.match.params.page}`
-            this.props.fetchPets( url )
-        }
-        else {
-            let url = `https://api.petfinder.com/v2/animals?location=${this.props.match.params.city},%20${this.props.match.params.state}&type=dog`
-            this.props.fetchPets( url )
-        }
-        
-    }
-
-    componentDidUpdate() {
-        // if(!!this.props.pets[0]){
-        //   let path = this.props.location.pathname.split('/')
-        //   debugger
-        //   if( path.slice(-1)[0].includes('page') ) {
-        //     let params = Object.assign( {}, {city: path[path.length - 2], state: path[path.length - 3], page: path[path.length - 1]} )
-        //     let url = `https://api.petfinder.com/v2/animals?location=${params.city},%20${params.state}&type=dog/${params.page}`
-        //     this.props.fetchPets( url )
-        // }
-        // else {
-        //     let params = Object.assign( {}, {city: path[path.length - 1], state: path[path.length - 2] } )
-        //     let url = `https://api.petfinder.com/v2/animals?location=${params.city},%20${params.state}&type=dog`
-        //     this.props.fetchPets( url )
-        // }
-        //     console.log('update', path)
-        //     debugger
-        //     let cityAndState = Object.assign( {}, {city: path[path.length - 1], state: path[path.length - 2] } )
-        //     let param = `https://api.petfinder.com/v2/animals?location=${cityAndState.city},%20${cityAndState.state}&type=dog`
-        // }
-        // this.props.fetchPets( param )
+        let url = `https://api.petfinder.com/v2/animals?location=${this.props.match.params.city},%20${this.props.match.params.state}&type=dog${this.props.match.params.page}`
+        this.props.fetchPets( url )
     }
 
     renderPhoto = (pet) => {
@@ -77,6 +43,5 @@ const mapStateToProps = state => {
         pagination: state.pagination
     }
 }
-
 
 export default connect (mapStateToProps, { fetchPets } ) (PetsContainer)
