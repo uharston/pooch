c51e6c1f17540bfafd75a03a807a75ddde1db91c
uharston <uharston@gmail.com>
2020-09-24 15:46:27 -0500

    too many changes

--- front-end/src/components/LandingPageSearchBar.js
+++ front-end/src/components/LandingPageSearchBar.js
@@ -1,35 +1,68 @@
 import React, { Component } from 'react'; 
 import { withRouter } from 'react-router-dom'; 
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 
 class LandingPageSearchBar extends Component {
 
     state = {
-        query: ''
+        breed: '',
+        location: ''
     }
 
-    handleChange = (event) => {
+    handleBreedChange = (event) => {
         this.setState({
-            query: event.target.value
+            breed: event.target.value 
         })
     }
 
+    handleLocationChange = (event) => {
+        this.setState({
+            location: event.target.value
+        })
+    }
+
+    handleGoogleAutocomplete = (event) => {
+        debugger
+    }
+
+
+
     handleSubmit = (event) => {
         event.preventDefault();
-        let [city, state] = this.state.query.split(',')
+        const place = event.target.getElementsByClassName('css-1uccc91-singleValue')[0].textContent
+        let [city, state] = place.split(', ' ).slice(-3)
         let cityAndState = `${ state.toLowerCase().trim() }/${ city.toLowerCase().trim() }`
         this.props.history.push(`search/${cityAndState}/&page=1`)
     }
 
     render() {
+        
         return(
             <div className="landing-page-search-bar">
                 <p> Search by: </p> 
                 <button>Breed</button>
                 <button>Location</button>
                 <form onSubmit={ (event) => this.handleSubmit(event) }>
-                    <label>Enter location: </label>
-                    <input type='text' onChange={ (e) => this.handleChange(e) } />
+                    <label>Breed:</label>
+                    <input type="text" onChange={ (e) => this.handleBreedChange(e)} /> 
+
+
+
+                    <GooglePlacesAutocomplete 
+                        apiKey="AIzaSyAogTqS8y_uMthvPq5C16K8RRLqLKqG5sE" 
+                        autocompletionRequest={{
+                            componentRestrictions: {
+                                country: ['us', 'ca']
+                            }
+                        }}  
+                    />
+
+
+
+
+                    {/* <label>Enter location: </label>
+                    <input type='text' onChange={ (e) => this.handleLocationChange(e) } /> */}
                     <input type='submit' value='Go'/>
                 </form>
             </div>
