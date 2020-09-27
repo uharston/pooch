3ca2f4031ba8375391a71fc5a6afb4318b6c804e
uharston <uharston@gmail.com>
2020-09-27 15:48:18 -0500

    Added Login buttons

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 // import logo from '../logo-black.svg';
+import LoginBtn from './LoginBtn'
+import LogoutBtn from './LogoutBtn'
 
 
 class NavBar extends Component {
@@ -10,9 +12,8 @@ class NavBar extends Component {
                 {/* <img style={{width: '20px'}} src={logo} /> */}
                 {/* <NavLink to='/'>Home</NavLink>
                 <NavLink to='#'>Log In</NavLink> */}
-                <form>
-                    <input type='submit' value='Log In' />
-                </form>
+                <LoginBtn /> 
+                <LogoutBtn />
             </div>
         )
     }
