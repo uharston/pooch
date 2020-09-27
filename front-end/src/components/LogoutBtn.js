95bce6f1a5d1ae73050d49daee749d68dc5241cb
uharston <uharston@gmail.com>
2020-09-27 18:44:43 -0500

    Abstract oauth key

--- front-end/src/components/LogoutBtn.js
+++ front-end/src/components/LogoutBtn.js
@@ -1,8 +1,6 @@
 import React from 'react' 
 import { GoogleLogout } from 'react-google-login';
 
-const clientId = 'YOUR_CLIENT_ID.apps.googleusercontent.com';
-
 function LogoutBtn() {
     const onSuccess = () => {
         alert('Logout successful')
@@ -10,11 +8,11 @@ function LogoutBtn() {
 
     return(
         <div className='logout-btn'>
-            <GoogleLogout
-                clientId={clientId}
+            {/* <GoogleLogout
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
                 buttonText="Logout"
                 onLogoutSuccess={onSuccess}
-            />
+            /> */}
         </div>
     )
 }
