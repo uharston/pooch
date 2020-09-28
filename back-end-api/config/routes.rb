9ab4bf0a46120a7f5c175906e992526d0d4861c5
uharston <uharston@gmail.com>
2020-09-27 20:49:08 -0500

    Clean up routes

--- back-end-api/config/routes.rb
+++ back-end-api/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    resources :breeds, only: %i[index show update]
-  get '/auth/:provider/callback' => 'sessions#omniauth'
-  #  resource :users, only: [:create]
-   post "/login", to: "auth#login" # <= Probably the most useful route 
-  #  get "/auto_login", to: "auth#auto_login"
-  #  get "/user_is_authed", to: "auth#user_is_authed"
+  post '/login' => 'sessions#omniauth'
+
 end
