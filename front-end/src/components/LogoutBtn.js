69cbe3612aff7ae5ebf3e3c5922f92a6bd39570b
uharston <uharston@gmail.com>
2020-09-27 20:50:54 -0500

    Connect to store | Dispatch method

--- front-end/src/components/LogoutBtn.js
+++ front-end/src/components/LogoutBtn.js
@@ -1,20 +1,26 @@
-import React from 'react' 
+import React, { Component } from 'react'; 
+import { connect } from 'react-redux'; 
 import { GoogleLogout } from 'react-google-login';
+import { fetchLogout } from '../actions/loginAction';
 
-function LogoutBtn() {
-    const onSuccess = () => {
+
+class LogoutBtn extends Component {
+    onSuccess = (response) => {
+        this.props.fetchLogout()
         alert('Logout successful')
     }
 
-    return(
-        <div className='logout-btn'>
-            {/* <GoogleLogout
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                buttonText="Logout"
-                onLogoutSuccess={onSuccess}
-            /> */}
-        </div>
-    )
+    render() {
+        return(
+            <div className='logout-btn'>
+                <GoogleLogout
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    buttonText="Logout"
+                    onLogoutSuccess={this.onSuccess}
+                />
+            </div>
+        )
+    }
 }
 
-export default LogoutBtn; 
+export default connect(null, { fetchLogout }) (LogoutBtn); 
