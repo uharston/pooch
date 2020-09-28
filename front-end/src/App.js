f3cd840fc22845cc284699d15947f39ff990992b
uharston <uharston@gmail.com>
2020-09-28 11:40:19 -0500

    Import bootstap | Rename Navber Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
   BrowserRouter as Router, 
   Route 
 } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import Navigation from './components/Navigation';
 import LandingPageContainer from './containers/LandingPageContainer';
 import PetsContainer from './containers/PetsContainer'
 
@@ -14,10 +16,9 @@ function App() {
   return (
     <Router> 
       <div className="App">
-        <NavBar />
+        <Navigation />
         <Route exact path="/" component={ LandingPageContainer } />
         <Route path='/search/:breed/:state/:city/:page' render={ routerProps => <PetsContainer {...routerProps} /> } />
-        
       </div>
     </Router>
 
