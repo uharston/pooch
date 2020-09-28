b945c1bcd8edc82a964f9ce3d8950115644af18c
uharston <uharston@gmail.com>
2020-09-28 16:53:09 -0500

    Add profile image and dropdown menu

--- front-end/src/components/LogoutBtn.js
+++ front-end/src/components/LogoutBtn.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux'; 
+import { Image, Dropdown } from 'react-bootstrap';
+
 import { GoogleLogout } from 'react-google-login';
 import { fetchLogout } from '../actions/loginAction';
 
@@ -11,13 +13,24 @@ class LogoutBtn extends Component {
     }
 
     render() {
+        
         return(
             <span className='logout-btn'>
-                <GoogleLogout
-                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                    buttonText="Logout"
-                    onLogoutSuccess={this.onSuccess}
-                />
+                <Dropdown>
+                    <Dropdown.Toggle>
+                        <Image className='profile-img' src={this.props.user.image_url} roundedCircle />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href='#'>Favorites</Dropdown.Item>
+                        <Dropdown.Item >
+                        <GoogleLogout
+                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                            buttonText="Logout"
+                            onLogoutSuccess={this.onSuccess}
+                        />
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </span>
         )
     }
