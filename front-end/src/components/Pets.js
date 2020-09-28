76c4ce0729b936d34c83d7654e4763ddbf9d785f
uharston <uharston@gmail.com>
2020-09-28 16:52:30 -0500

    Abstract code to Pet Component

--- front-end/src/components/Pets.js
+++ front-end/src/components/Pets.js
@@ -1,38 +1,15 @@
 import React, { Component } from 'react'; 
 import {Container, Row, Card, Col, Button } from 'react-bootstrap'; 
+import Pet from './Pet';
 
 class Pets extends Component {
 
-    renderPhoto = (pet) => {
-        return pet.primary_photo_cropped ? <Card.Img variant="top" src={pet.primary_photo_cropped.small} /> : <p> NO PHOTO FOUND </p>
-    }
-
-    renderBody = (pet) => {
-        return (
-            <Card.Body>
-                <Card.Title>{pet.name}</Card.Title>
-                    <Card.Text>
-                        {pet.age} {pet.breeds.mixed ? pet.breeds.primary + ' Mix' : pet.breeds.primary }
-                    </Card.Text>
-                    <Button variant="primary">More Info...</Button>
-            </Card.Body>
-
-        )
-    }
-
     render() {
-        console.log(this.props.pets)
         return(
             <div className='pets'>
                 <Container>
-                    <Row xs={1} md={3} lg={3} >
-                    { this.props.pets.map( pet => 
-                            <Col >
-                                <Card>
-                                    {this.renderPhoto(pet)}
-                                    {this.renderBody(pet)}
-                                </Card>
-                            </Col>)}
+                    <Row xs={1} md={3} lg={3} >   
+                        { this.props.pets.map( pet => <Pet pet={pet}/> ) }
                     </Row>
                 </Container>
             </div>
