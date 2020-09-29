53af643b51adf32b069bcdcc6f1899b24c74f23c
uharston <uharston@gmail.com>
2020-09-29 14:13:16 -0500

    Fetch favorite pets | Pass as props to Pets

--- front-end/src/containers/FavoritesContainer.js
+++ front-end/src/containers/FavoritesContainer.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'; 
+import { connect } from 'react-redux'; 
+import { getFavoritePets } from '../actions/favoritesAction'
+import Pets from '../components/Pets';
+
 
 class FavoritesContainer extends Component {
+
+    componentDidUpdate() {
+        if(this.props.user.logged_in) {
+            this.props.user.favorite_ids.map( fav => {
+                this.props.getFavoritePets(`https://api.petfinder.com/v2/animals/${fav.petfinder_id}`)
+            })
+        }
+    }
+
+ 
     
     render() {
+        
         return(
             <div className='favorites-container'>
+                <Pets pets={this.props.user.favorite_pets} />
 
+                {/* {this.props.user.favorite_pets.map( pet => <p>{pet.name}</p>)} */}
             </div>
         )
     }
 }
-export default FavoritesContainer
+
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+export default connect(mapStateToProps, { getFavoritePets }) (FavoritesContainer)
