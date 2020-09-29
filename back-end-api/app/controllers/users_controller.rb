bbf0f1144b6832541f2f1f831cc42c1ccd14aa86
uharston <uharston@gmail.com>
2020-09-29 00:53:25 -0500

    Update action

--- back-end-api/app/controllers/users_controller.rb
+++ back-end-api/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
  
- def update 
+    def update 
+        user = User.find_by(email: params[:user][:email])
+        if user.valid? 
+            pet = user.pets.create(petfinder_id: params[:pet][:id], url: params[:pet][:url] )
+            render json: { message: "Dog Saved!", pet_id: pet.petfinder_id }
+        end
+    end 
 
- end 
 end
