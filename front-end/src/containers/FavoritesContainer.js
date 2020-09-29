7e7430e50ca4c31a91fb5d0834e37dd91b9db5e1
uharston <uharston@gmail.com>
2020-09-29 11:26:20 -0500

    Made Favorites Container

---
+++ front-end/src/containers/FavoritesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+class FavoritesContainer export Component {
+    
+    render() {
+        return(
+            <div className='favorites-container'>
+
+            </div>
+        )
+    }
+}
+export default FavoritesContainer
