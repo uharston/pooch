19317ca9548d7ee7cc06f3e7710a9f1a472e8718
uharston <uharston@gmail.com>
2020-10-01 08:47:32 -0500

    Finished house-trained filter

--- front-end/src/containers/PetsContainer.js
+++ front-end/src/containers/PetsContainer.js
@@ -9,28 +9,32 @@ import Pets from '../components/Pets';
 class PetsContainer extends Component {
 
     state = {
-        
+        houseTrained: false 
     }
 
     componentDidMount() {
         let breed 
         this.props.match.params.breed === 'all-dogs' ? breed = '' : breed = this.props.match.params.breed 
         let url = `https://api.petfinder.com/v2/animals?location=${this.props.match.params.city},%20${this.props.match.params.state}&type=dog${this.props.match.params.page}&breed=${breed}`
-        console.log('a')
         this.props.fetchPets( url )
-        console.log('b')
     }
 
     handleClick = () => {
-        console.log(this.props)
-        console.log(this.props.pets.filter( pet => pet.attributes.spayed_neutered === true ))
+        this.setState( prevState => ({
+            houseTrained: !prevState.houseTrained 
+        }))
+
+    }
+
+    filterProps = () => {
+        return this.state.houseTrained ? this.props.pets.filter( pet => pet.attributes.spayed_neutered === true ) : this.props.pets
     }
 
     render() {
         return(
             <div className='search-container'>
                 <button onClick={ () => this.handleClick() } >House-Trained </button>
-                <Pets pets={ this.props.pets } />
+                <Pets pets={ this.filterProps() } />
                 <PetsPagination pagination={this.props.pagination} fetchPets={this.props.fetchPets} /> 
             </div>
         )
