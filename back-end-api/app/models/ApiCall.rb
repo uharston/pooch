fcc1c629eeaca03275d2194f6a391accc43e6c2f
uharston <uharston@gmail.com>
2020-10-01 20:08:42 -0500

    fixed math

--- back-end-api/app/models/ApiCall.rb
+++ back-end-api/app/models/ApiCall.rb
@@ -1,7 +1,7 @@
 class ApiCall < ApplicationRecord 
 
     def expired? 
-       time_left =  (self.updated_at.to_f * 1000) - (self.expires_in * 1000)
-       time_left < 1 ? true : false 
+        time_left = self.expires_in - (Time.now.to_f * 1000)
+       time_left < 5 ? true : false 
     end 
 end 
