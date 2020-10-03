5e8b9d5e94858fd48a44be1aa1fdd4f466f542fc
uharston <uharston@gmail.com>
2020-10-03 15:43:44 -0500

    Deleted un-needed routes

--- back-end-api/config/routes.rb
+++ back-end-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :breeds, only: %i[index show update]
+  resources :breeds, only: %i[index]
   post '/user' => 'users#update'
   post '/login' => 'sessions#omniauth'
   post '/pets' => 'api_calls#petfinder'
