6c30102a704bf8db97f498a2d2bb97af9a5ec257
uharston <uharston@gmail.com>
2020-10-23 16:08:42 -0500

    npm run build

---
+++ front-end/build/precache-manifest.596e7b29c12e839f3283e22cd7d45a21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5920c7b543fd7cdc8745e606b5d3fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d95d77b97c736fc89c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dc74eb6fa13e6b4cfb7d",
+    "url": "/static/css/main.bcb23ce0.chunk.css"
+  },
+  {
+    "revision": "72d95d77b97c736fc89c",
+    "url": "/static/js/2.b85469fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b85469fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc74eb6fa13e6b4cfb7d",
+    "url": "/static/js/main.d5599c5f.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "82ae637bbbee12898d68cdec04d0be06",
+    "url": "/static/media/dog_background_2.82ae637b.jpg"
+  },
+  {
+    "revision": "c88a36d0a9dcd73b2735877edac0acd5",
+    "url": "/static/media/logo-black.c88a36d0.svg"
+  }
+]);
