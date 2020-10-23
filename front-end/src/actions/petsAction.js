7333e84baed970c63548cca33e537659f3b01450
uharston <uharston@gmail.com>
2020-10-23 16:45:24 -0500

    Change fetch url

--- front-end/src/actions/petsAction.js
+++ front-end/src/actions/petsAction.js
@@ -2,7 +2,7 @@ import  axios  from 'axios';
 
 export const fetchBreeds = () => {
     return (dispatch) => {
-        fetch("http://127.0.0.1:4000/breeds")
+        fetch("https://safe-cliffs-66235.herokuapp.com/breeds")
         .then(response => response.json())
         .then(breeds => dispatch({ type: 'ADD_BREEDS', breeds}))
     }
@@ -13,7 +13,7 @@ export const fetchPets = function(param) {
     return function(dispatch)  {
         
         dispatch({ type: 'LOADING_PETS' })
-        axios.post("http://127.0.0.1:4000/pets", {url: param}, {withCredentials: true} )
+        axios.post("https://safe-cliffs-66235.herokuapp.com/pets", {url: param}, {withCredentials: true} )
             .then( petResponse => {
                 
                 console.log(petResponse)
