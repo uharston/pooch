bfab293a9dfc5b238d38aaac771a9c0e5e1bc181
uharston <uharston@gmail.com>
2020-11-30 14:05:37 -0600

    Add api_token to json response for debugging

--- back-end-api/app/controllers/api_calls_controller.rb
+++ back-end-api/app/controllers/api_calls_controller.rb
@@ -25,7 +25,7 @@ class ApiCallsController < ApplicationController
         # else 
         #     render json: {petfinder_response: "Api call error!" }
         # end
-        render json: {petfinder_response: petfinder.name }
+        render json: {petfinder_response: petfinder.name, api_token: petfinder.api_token }
 
     end 
     
