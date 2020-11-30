65e9f7fea126c9df51c5b4d748f6fec16707c8fc
uharston <uharston@gmail.com>
2020-11-30 11:15:53 -0600

    Permit localhost to be an origin

--- back-end-api/config/initializers/cors.rb
+++ back-end-api/config/initializers/cors.rb
@@ -23,7 +23,16 @@
 #   end
   Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'https://pooch.netlify.app, http://localhost:3000/'
+      origins 'https://pooch.netlify.app'
+    
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+
+    allow do
+      origins 'http://localhost:3000'
     
       resource '*',
         headers: :any,
