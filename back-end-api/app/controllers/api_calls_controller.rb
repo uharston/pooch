81a4e05b6921abe10a2e6189102e4db30b9e5aca
uharston <uharston@gmail.com>
2020-11-30 14:01:07 -0600

    Comment out petfinder action body for debugging

--- back-end-api/app/controllers/api_calls_controller.rb
+++ back-end-api/app/controllers/api_calls_controller.rb
@@ -3,28 +3,30 @@ class ApiCallsController < ApplicationController
 
     def petfinder 
         petfinder = ApiCall.find_by(name: 'petfinder') 
-        if !petfinder 
-            render json: {petfinder_response: "Could not find a petfinder instance in database!" }
-        end
-        if !petfinder.api_token || petfinder.expired? 
-            token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
-                req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-                req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
-            end
-            data = JSON.parse(token_response.body)
-            expires = (Time.now.to_f * 1000) + (data["expires_in"] * 1000)
+        # if !petfinder 
+        #     render json: {petfinder_response: "Could not find a petfinder instance in database!" }
+        # end
+        # if !petfinder.api_token || petfinder.expired? 
+        #     token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
+        #         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        #         req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
+        #     end
+        #     data = JSON.parse(token_response.body)
+        #     expires = (Time.now.to_f * 1000) + (data["expires_in"] * 1000)
             
-            petfinder.update(api_token: data["access_token"], expires_in: expires ) 
-        end
+        #     petfinder.update(api_token: data["access_token"], expires_in: expires ) 
+        # end
         
-        resp = Faraday.get( params[:url] ) do |req| 
-            req.headers['Authorization'] = "Bearer #{petfinder.api_token}"
-        end
-        if resp 
-            render json: {petfinder_response: JSON.parse(resp.body) }
-        else 
-            render json: {petfinder_response: "Api call error!" }
-        end
+        # resp = Faraday.get( params[:url] ) do |req| 
+        #     req.headers['Authorization'] = "Bearer #{petfinder.api_token}"
+        # end
+        # if resp 
+        #     render json: {petfinder_response: JSON.parse(resp.body) }
+        # else 
+        #     render json: {petfinder_response: "Api call error!" }
+        # end
+        render json: {petfinder_response: petfinder.name }
+
     end 
     
     def google_places_search
