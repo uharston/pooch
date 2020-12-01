83f1dc32a04b690086009eb3750f096f527eab21
uharston <uharston@gmail.com>
2020-12-01 10:46:17 -0600

    Corrected response return

--- back-end-api/app/controllers/api_calls_controller.rb
+++ back-end-api/app/controllers/api_calls_controller.rb
@@ -2,15 +2,9 @@ require 'faraday'
 class ApiCallsController < ApplicationController 
 
     def petfinder 
-        
+        # Find Petfinder Instance 
         petfinder = ApiCall.find_by(name: 'petfinder') 
-        #     token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
-        #         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        #         req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
-        #     end
-        # if !petfinder 
-        #     render json: {petfinder_response: "Could not find a petfinder instance in database!" }
-        # end
+        # Check for valid oauth token 
         if !petfinder.api_token || petfinder.expired? 
             token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
                 req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
@@ -18,29 +12,19 @@ class ApiCallsController < ApplicationController
             end
             data = JSON.parse(token_response.body)
             expires = (Time.now.to_i * 1000) + (data["expires_in"] * 1000)
-            
             petfinder.update(api_token: data["access_token"], expires_in: expires ) 
         end
-        
+        # Fetch Pets 
         resp = Faraday.get( params[:url] ) do |req| 
             req.headers['Authorization'] = "Bearer #{petfinder.api_token}"
         end
         if resp 
-            # render json: {petfinder_response: JSON.parse(resp.body) }
-            render json: {petfinder_response: petfinder.name, api_token: petfinder.api_token, token_response: token_response }
+            render json: {petfinder_response: JSON.parse(resp.body) }
         else 
             render json: {petfinder_response: "Api call error!" }
         end
-        # render json: {petfinder_response: 'hello'}
-
+ 
     end 
     
-    def google_places_search
-
-    end 
-
 end 
-
-# curl -d "grant_type=client_credentials&client_id={CLIENT-ID}&client_secret={CLIENT-SECRET}" https://api.petfinder.com/v2/oauth2/token
-# curl -d "grant_type=client_credentials&client_id=70TgEtMrkn14Jh5jt2TjSW2VCMDoVLwXPWF7VxfXMhFQ2SfJus&client_secret=L8OzS0UL8pwnrYwFtuDiADZ4sBSr3HOlIK5SkjK3" https://api.petfinder.com/v2/oauth2/token
 
