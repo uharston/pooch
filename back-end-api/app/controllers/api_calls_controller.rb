d2a42c1d36730745b69aea54dcecc6f5c718b8f3
uharston <uharston@gmail.com>
2020-12-01 10:13:49 -0600

    ...

--- back-end-api/app/controllers/api_calls_controller.rb
+++ back-end-api/app/controllers/api_calls_controller.rb
@@ -12,10 +12,10 @@ class ApiCallsController < ApplicationController
         #     render json: {petfinder_response: "Could not find a petfinder instance in database!" }
         # end
         # if !petfinder.api_token || petfinder.expired? 
-            token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
-                req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-                req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
-            end
+            # token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
+            #     req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            #     req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
+            # end
             # data = JSON.parse(token_response.body)
             # expires = (Time.now.to_f * 1000) + (data["expires_in"] * 1000)
             
@@ -30,7 +30,7 @@ class ApiCallsController < ApplicationController
         # else 
         #     render json: {petfinder_response: "Api call error!" }
         # end
-        render json: {petfinder_response: token_response}
+        render json: {petfinder_response: ENV['PETFINDER_CLIENT_ID']}
         # render json: {petfinder_response: petfinder.name, api_token: petfinder.api_token, token_response: token_response }
 
     end 
