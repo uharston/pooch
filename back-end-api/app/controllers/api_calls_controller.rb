de92d4a9a577b7a249064cc5ec419a51646103e7
uharston <uharston@gmail.com>
2020-12-01 10:04:10 -0600

    Add oauth token request for testing

--- back-end-api/app/controllers/api_calls_controller.rb
+++ back-end-api/app/controllers/api_calls_controller.rb
@@ -11,16 +11,16 @@ class ApiCallsController < ApplicationController
         # if !petfinder 
         #     render json: {petfinder_response: "Could not find a petfinder instance in database!" }
         # end
-        # if !petfinder.api_token || petfinder.expired? 
-        #     token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
-        #         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        #         req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
-        #     end
-        #     data = JSON.parse(token_response.body)
-        #     expires = (Time.now.to_f * 1000) + (data["expires_in"] * 1000)
+        if !petfinder.api_token || petfinder.expired? 
+            token_response = Faraday.post("https://api.petfinder.com/v2/oauth2/token" ) do |req| 
+                req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+                req.body = "grant_type=client_credentials&client_id=#{ENV['PETFINDER_CLIENT_ID']}&client_secret=#{ENV['PETFINDER_CLIENT_SECRET']}"  
+            end
+            data = JSON.parse(token_response.body)
+            expires = (Time.now.to_f * 1000) + (data["expires_in"] * 1000)
             
-        #     petfinder.update(api_token: data["access_token"], expires_in: expires ) 
-        # end
+            petfinder.update(api_token: data["access_token"], expires_in: expires ) 
+        end
         
         # resp = Faraday.get( params[:url] ) do |req| 
         #     req.headers['Authorization'] = "Bearer #{petfinder.api_token}"
